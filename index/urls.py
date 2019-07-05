a8ddf0c5a2f7de3c1554aa301da72e42e1a9258d
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-07-05 17:33:50 +0300

    add permissions

--- index/urls.py
+++ index/urls.py
@@ -18,9 +18,10 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 
 urlpatterns = [
-    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pai'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='Obtain a token'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='Token refresh'),
     path('books/', include('book.urls')),
     path('users/', include('user.urls')),
 ]

b3d4d318cbbd2faa40e2d8cb53bee38f9774240d
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-07-07 02:33:31 +0300

    added permissions

--- index/urls.py
+++ index/urls.py
@@ -3,10 +3,12 @@ from django.urls import (path, include)
 from rest_framework_simplejwt.views import (
     TokenObtainPairView, TokenRefreshView)
 
+
 urlpatterns = [
+    path('', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('api/token', TokenObtainPairView.as_view()),
-    path('api/token/refresh', TokenRefreshView.as_view()),
     path('users/', include('user.urls')),
     path('books/', include('book.urls')),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
 ]
